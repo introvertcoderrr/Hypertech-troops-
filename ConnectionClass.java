53842650adb74016b75b5360929e22048cdb1cdd
Anurag Yadav <166040440+introvertcoderrr@users.noreply.github.com>
2024-11-10 17:35:38 +0530

    Add files via upload
    --> Class Declaration:
           The class ConnectionClass is designed to handle MySQL database connections.
       
    -->Attributes:
         connection con: Holds the database connection object.
         Statement stm: Used to execute SQL queries.
    
    -->Constructor (ConnectionClass()):
        Load JDBC Driver: Class.forName("com.mysql.cj.jdbc.Driver"); loads the MySQL JDBC driver.
        Establish Connection: DriverManager.getConnection() connects to the MySQL database at localhost:3306/ams using the root username and the password 
        Create Statement: con.createStatement() creates a statement to execute SQL queries.
    
    -->Exception Handling:
          Any exceptions (like connection errors) are caught using a try-catch block and the error stack is printed for debugging.
    
    -->Main Method (main(String[] args)):
         The main method tests the connection by creating an instance of the ConnectionClass, which triggers the connection process.
         This class provides a basic setup for connecting to a MySQL database in a Java application.

---
+++ ConnectionClass.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AMS;
+
+/**
+ *
+ * @author PC
+ */
+
+import java.sql.*;
+public class ConnectionClass
+{
+    Connection con;
+    Statement stm;
+    
+    ConnectionClass()
+    {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ams","root","Ravendra0987@");
+            stm=con.createStatement();
+            
+            
+        }
+        catch(Exception ex){
+            ex.printStackTrace();
+        }
+        
+       
+         
+        
+        
+    
+    }
+    public static void main(String[] args)
+    {
+        new ConnectionClass();
+    
+    
+    } 
+        
+}
